9d118bf88f4a981d7e219c26b31ee37cd7e970d1
heraldo4 <104959335+heraldo4@users.noreply.github.com>
2023-11-26 23:19:59 -0600

    Servicios

--- servicios.php
+++ servicios.php
@@ -1,43 +1,51 @@
 <?php include "header.php" ?>
 
-<h1 class="titleServicios"> Servicios </h1>
-<div class="container-fluid text-center">
-    <h2>SERVICES</h2>
-    <h4>What we offer</h4>
+<h1 class="titleServicios"> Nuestros Servicios </h1>
+<div class="container text-center">
+    <div class="servicesImage" style="background-image: url('img/Servicios/Servicios2.jpg");>
+        <h4 class="greenbite-subtitle">Bienvenido a GreenBite, tu destino para una alimentación saludable y deliciosa. Nos enorgullece ofrecer una variedad de servicios diseñados para satisfacer tus necesidades nutricionales, sin comprometer el sabor. Descubre cómo hacemos que la comida saludable sea fácil, conveniente y deliciosa.</h4>
+    </div> 
     <br>
     <div class="row">
-        <div class="col-sm-4">
-            <span class="glyphicon glyphicon-off"></span>
-            <h4>POWER</h4>
-            <p>Lorem ipsum dolor sit amet..</p>
+        <div class="col-sm mb-4 d-flex align-items-center">
+            <i class="fa-solid fa-store fa-2xl"></i>
+            <div><h5>Menú Nutritivo</h5>
+                <p>Explora nuestro extenso menú de opciones saludables, cuidadosamente elaborado para adaptarse a diversos gustos y preferencias. Desde ensaladas frescas hasta platos calientes y opciones vegetarianas, nuestro menú ofrece una gama de delicias saludables para cada ocasión.</p>
+            </div>
         </div>
-        <div class="col-sm-4">
-            <span class="glyphicon glyphicon-heart"></span>
-            <h4>LOVE</h4>
-            <p>Lorem ipsum dolor sit amet..</p>
+        <div class="col-sm mb-4 d-flex align-items-center">
+            <i class="fa-solid fa-motorcycle fa-2xl"></i>
+            <div><h5>Envío Rápido y Seguro</h5>
+                <p>Facilitamos aún más tu experiencia al ofrecer un servicio de entrega rápido y seguro. Desde la comodidad de tu hogar hasta tu oficina, aseguramos que tus comidas saludables lleguen frescas y listas para disfrutar.</p>
+            </div>
         </div>
-        <div class="col-sm-4">
-            <span class="glyphicon glyphicon-lock"></span>
-            <h4>JOB DONE</h4>
-            <p>Lorem ipsum dolor sit amet..</p>
+        <div class="col-sm mb-4 d-flex align-items-center">
+            <i class="fa-solid fa-heart-pulse fa-2xl"></i>
+            <div><h5>Compromiso con la Salud</h5>
+                <p>Nuestra misión es promover la elección consciente de alimentos y fomentar un estilo de vida equilibrado. Nos comprometemos a ofrecer ingredientes frescos, calidad y un servicio excepcional para garantizar que cada experiencia con nosotros sea positiva.</p>
+            </div> 
         </div>
     </div>
     <br><br>
+    <h3 class="subtitle"> ¿Por Qué Elegir GreenBite?</h3>
     <div class="row">
-        <div class="col-sm-4">
-            <span class="glyphicon glyphicon-leaf"></span>
-            <h4>GREEN</h4>
-            <p>Lorem ipsum dolor sit amet..</p>
+        <div class="col-sm mb-4 d-flex align-items-center">
+            <i class="fa-solid fa-store fa-2xl"></i>
+            <div><h5>Variedad Nutritiva: </h5>
+                <p>Ofrecemos una amplia variedad de opciones nutritivas para satisfacer tus necesidades dietéticas.</p>
+            </div>
         </div>
-        <div class="col-sm-4">
-            <span class="glyphicon glyphicon-certificate"></span>
-            <h4>CERTIFIED</h4>
-            <p>Lorem ipsum dolor sit amet..</p>
+        <div class="col-sm mb-4 d-flex align-items-center">
+            <i class="fa-solid fa-lemon fa-2xl"></i>
+            <div><h5>Calidad Garantizada: </h5>
+                <p>Nos esforzamos por mantener los más altos estándares de calidad en cada ingrediente que utilizamos.</p>
+            </div>
         </div>
-        <div class="col-sm-4">
-            <span class="glyphicon glyphicon-wrench"></span>
-            <h4>HARD WORK</h4>
-            <p>Lorem ipsum dolor sit amet..</p>
+        <div class="col-sm mb-4 d-flex align-items-center">
+            <i class="fa-solid fa-users fa-2xl"></i>
+            <div><h5>Servicio Conveniente: </h5>
+                <p>Con nuestro servicio de entrega rápido, disfruta de comidas saludables directamente en tu puerta.</p>
+            </div>
         </div>
     </div>
 </div>
