a3166c402816b62a2f8a015b0dd146ce6ac945fd
heraldo4 <104959335+heraldo4@users.noreply.github.com>
2023-11-27 01:03:47 -0600

    404

--- contactenos.php
+++ contactenos.php
@@ -2,12 +2,12 @@
 
 <!-- Mapa -->
 <iframe class="justify-content-fluid" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31439.843319661682!2d-84.1339875390225!3d9.935587290166964!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0e342c50d15c5%3A0xe6746a6a9f11b882!2sSan%20Jos%C3%A9%20Province%2C%20San%20Jos%C3%A9!5e0!3m2!1sen!2scr!4v1701026982667!5m2!1sen!2scr" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    <main class="container">
+<main class="container">
     <section class="mb-4">
         <!--Section heading-->
-        <div class="contact form-title" style="background-image: url('img/Contactenos/Vegetales.jpeg");>
+        <div class="contact form-title" style="background-image: url('img/Contactenos/Vegetales.jpeg" );>
             <h4 class="text-center my-4">Contactenos</h4>
-        </div> 
+        </div>
         <!--Section description-->
         <section class="row">
             <!-- Formulario -->
@@ -16,11 +16,19 @@
                     <input type="text" class="contact form-control" name="nombre" id="nombre" placeholder="Digite su Nombre">
                     <label for="nombre">Nombre</label>
                 </div>
-                <div class="form-floating mb-3">
-                    <input type="tel" class="contact form-control" name="telefono" id="telefono" placeholder="Telefono">
-                    <label for="telefono">Telefono</label>
+
+                <div class="row mb-3">
+                    <div class="col-lg-6">
+                        <label class="form-floating mb-3" for="email"></label>
+                        <input type="email" class="contact form-control" name="email" id="email" placeholder="Correo electrónico">
+                    </div>
+                    <div class="col-lg-6">
+                        <label class="form-floating mb-3" for="telefono"></label>
+                        <input type="phone" class="contact form-control" name="telefono" id="telefono" placeholder="Telefono">
+                    </div>
                 </div>
-                <div class="mb-3">
+
+                <div class="mb-3 mensaje">
                     <label for="mensaje">Mensaje</label>
                     <textarea class="contact form-control" name="mensaje" id="mensaje" cols="30" rows="10"></textarea>
                 </div>
@@ -33,7 +41,7 @@
                 <ul class="list-unstyled mb-0">
                     <li>
                         <h3>Información</h3>
-                            <p>¿Tienes alguna pregunta o inquietud? ¡Estamos aquí para ayudarte! No dudes en contactarnos directamente. Nuestro equipo estará encantado de responder a tus consultas y brindarte la asistencia que necesitas. Completa el formulario a continuación o utiliza la información de contacto proporcionada. Nos comprometemos a responderte en cuestión de horas.</p>
+                        <p>¿Tienes alguna pregunta o inquietud? ¡Estamos aquí para ayudarte! No dudes en contactarnos directamente. Nuestro equipo estará encantado de responder a tus consultas y brindarte la asistencia que necesitas. Completa el formulario a continuación o utiliza la información de contacto proporcionada. Nos comprometemos a responderte en cuestión de horas.</p>
                     </li>
                     <li>
                         <i class="locacion fa-solid fa-location-dot fa-xl"></i>
@@ -41,11 +49,11 @@
                     </li>
                     <li>
                         <i class="telefono fa-solid fa-phone fa-xl"></i>
-                        <p>+ 01 234 567 89</p>
+                        <p>+506 2256 7832</p>
                     </li>
                     <li>
                         <i class="correo fa-solid fa-envelope fa-xl"></i>
-                        <p>contact@mdbootstrap.com</p>
+                        <p>info@greenbite.com</p>
                     </li>
                 </ul>
             </div>
