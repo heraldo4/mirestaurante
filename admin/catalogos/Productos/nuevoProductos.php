d54a3ea454ad94b88f86a1ec6a1c9401dac6b141
heraldo4 <104959335+heraldo4@users.noreply.github.com>
2023-12-01 20:00:19 -0600

    ADODB

--- admin/catalogos/Productos/nuevoProductos.php
+++ admin/catalogos/Productos/nuevoProductos.php
@@ -1,9 +1,9 @@
 <!-- Para enviar archivos es obligatorio el method="POST" y el enctype="multipart/form-data"-->
-<form id="frmPelicula" class="w-40 m-auto mx-5 my-5" action="" method="GET" enctype="multipart/form-data">
+<form id="frmProductos" class="w-40 m-auto mx-5 my-5" action="" method="GET" enctype="multipart/form-data">
     <input type="hidden" id="id" name="id">    
     <div class="d-flex justify-content-center bd-highlight mb-3">
             <div class="p-2 bd-highlight">
-                <a href="index.php?page=peliculas" class="btn btn-secondary"><i class="fas fa-arrow-left"></i> Regresar</a>
+                <a href="index.php?page=productos" class="btn btn-secondary"><i class="fas fa-arrow-left"></i> Regresar</a>
             </div>
 
             <div class="me-auto p-2 bd-highlight ">
@@ -20,8 +20,8 @@
             <label for="duracion">Costo</label>
         </div>
         <div class="form-floating mb-3">
-            <input type="text" class="form-control" name="director" id="director" placeholder="Director">
-            <label for="director">Tipo</label>
+            <input type="text" class="form-control" name="tipo" id="tipo" placeholder="Tipo">
+            <label for="tipo">Tipo</label>
         </div>
 
         <div class="form-floating mb-3">
