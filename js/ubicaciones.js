701449217f779b24c3032886397fe78dd8c1080a
heraldo4 <104959335+heraldo4@users.noreply.github.com>
2023-12-03 22:36:58 -0600

    Ubicaciones

--- js/ubicaciones.js
+++ js/ubicaciones.js
@@ -1,15 +1,60 @@
 // JSON con la información de restaurantes.
-// ejemplo:
 const restaurantes = [
     {
-        nombre: "Restaurante 1",
+        nombre: "GreenBite Limón",
         latitud: 10.079670787892237,      
         longitud: -83.18852258113952,
-        foto: "img/Portada/Inicio.jpg",
-        direccion: "Dirección 1",
-        telefono: "123-456-7890"
+        foto: "img/Ubicaciones/Limon.jpg",
+        direccion: "Ruta Nacional Treciaria 803, Limón, Matina",
+        telefono: "+506 2256 7832 ext. 01"
     },
-    // más restaurantes
+
+    {
+        nombre: "GreenBite San José",
+        latitud: 9.9453109,      
+        longitud: -84.0591027,
+        foto: "img/Ubicaciones/SanJose.jpg",
+        direccion: "San José, San José, Goicoechea, Guadalupe",
+        telefono: "+506 2256 7832 ext. 02"
+    },
+
+    {
+        nombre: "GreenBite Alajuela",
+        latitud: 10.017155,      
+        longitud: -84.216148,
+        foto: "img/Ubicaciones/Alajuela.jpg",
+        direccion: "Ave Tomás Guardia Alajuela",
+        telefono: "+506 2256 7832 ext. 03"
+    },
+
+    {
+        nombre: "GreenBite Heredia",
+        latitud: 9.998070,      
+        longitud: -84.118648,
+        foto: "img/Ubicaciones/Heredia.jpg",
+        direccion: "Heredia, Fatima",
+        telefono: "+506 2256 7832 ext. 04"
+    },
+
+    {
+        nombre: "GreenBite Cartago",
+        latitud: 9.864424,      
+        longitud: -83.917734,
+        foto: "img/Ubicaciones/Cartago.jpg",
+        direccion: "Avenida Central Provincia de Cartago, Cartago",
+        telefono: "+506 2256 7832 ext. 05"
+    },
+
+        {
+        nombre: "GreenBite Puntarenas",
+        latitud: 9.9791033,      
+        longitud: -84.7261737,
+        foto: "img/Ubicaciones/Puntarenas.jpg",
+        direccion: "Barranca, Puntarenas Province",
+        telefono: "+506 2256 7832 ext. 06"
+    },
+
+
 ];
 
 function initMap() {
@@ -17,6 +62,8 @@ function initMap() {
         center: { lat: restaurantes[0].latitud, lng: restaurantes[0].longitud },
         zoom: 10
     });
+
+    const infowindows = [];
 
     restaurantes.forEach(restaurante => {
         const marker = new google.maps.Marker({
@@ -36,8 +83,18 @@ function initMap() {
             `
         });
 
+        infowindows.push(infowindow);
+
         marker.addListener('click', function () {
+            // Cerrar todos los infowindows abiertos
+            infowindows.forEach(iw => iw.close());
             infowindow.open(map, marker);
+
+            // Centrar el mapa en el marcador clicado
+            map.setCenter(marker.getPosition());
+            map.setZoom(14);  // Puedes ajustar el valor del zoom según tus preferencias
         });
     });
 }
+
+
--- ubicaciones.php
+++ ubicaciones.php
@@ -2,18 +2,89 @@
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ubicaciones de Restaurantes</title>
     <link rel="stylesheet" href="css/ubicaciones.css">
 </head>
+
 <body>
     <div id="map"></div>
+
+    <!-- Contenedor para la información debajo del mapa -->
+    <div class="container" style="max-width: 1500px;">
+        <div class="info-row">
+            <!-- Primer Restaurante -->
+            <div class="info-box">
+                <img src="img/Ubicaciones/Limon.jpg" alt="GreenBite Limón" class="info-image">
+                <div class="info-details">
+                    <p><strong>GreenBite Limón</strong></p>
+                    <p>Dirección: Ruta Nacional Treciaria 803, Limón, Matina</p>
+                    <p>Teléfono: +506 2256 7832 ext. 01</p>
+                </div>
+            </div>
+
+            <!-- Segundo Restaurante -->
+            <div class="info-box">
+                <img src="img/Ubicaciones/SanJose.jpg" alt="GreenBite San José" class="info-image">
+                <div class="info-details">
+                    <p><strong>GreenBite San José</strong></p>
+                    <p>Dirección: San José, San José, Goicoechea, Guadalupe</p>
+                    <p>Teléfono: +506 2256 7832 ext. 02</p>
+                </div>
+            </div>
+
+            <!-- Tercer Restaurante -->
+            <div class="info-box">
+                <img src="img/Ubicaciones/Alajuela.jpg" alt="GreenBite Alajuela" class="info-image">
+                <div class="info-details">
+                    <p><strong>GreenBite Alajuela</strong></p>
+                    <p>Dirección: Ave Tomás Guardia Alajuela</p>
+                    <p>Teléfono: +506 2256 7832 ext. 03</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="info-row">
+            <!-- Cuarto Restaurante -->
+            <div class="info-box">
+                <img src="img/Ubicaciones/Heredia.jpg" alt="GreenBite Heredia" class="info-image">
+                <div class="info-details">
+                    <p><strong>GreenBite Heredia</strong></p>
+                    <p>Dirección: Heredia, Fatima</p>
+                    <p>Teléfono: +506 2256 7832 ext. 04</p>
+                </div>
+            </div>
+
+            <!-- Quinto Restaurante -->
+            <div class="info-box">
+                <img src="img/Ubicaciones/Cartago.jpg" alt="GreenBite Cartago" class="info-image">
+                <div class="info-details">
+                    <p><strong>GreenBite Cartago</strong></p>
+                    <p>Dirección: Avenida Central Provincia de Cartago, Cartago</p>
+                    <p>Teléfono: +506 2256 7832 ext. 05</p>
+                </div>
+            </div>
+
+
+            <!-- Sexto Restaurante -->
+            <div class="info-box">
+                <img src="img/Ubicaciones/Puntarenas.jpg" alt="GreenBite Puntarenas" class="info-image">
+                <div class="info-details">
+                    <p><strong>GreenBite Puntarenas</strong></p>
+                    <p>Dirección: Barranca, Puntarenas Province</p>
+                    <p>Teléfono: +506 2256 7832 ext. 06</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script src="js/ubicaciones.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDPWM3osMJTdxKFvEsqY2xy9mWWfW7PRh4&callback=initMap"></script>
 </body>
+
 </html>
 
 <?php include "footer.php" ?>
