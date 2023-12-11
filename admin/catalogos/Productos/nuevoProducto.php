e06fb786754c7ec6614745c027c5c696927713af
Alejimenez2003 <140208432+Alejimenez2003@users.noreply.github.com>
2023-12-10 20:56:14 -0500

    update
    update

--- admin/catalogos/Productos/nuevoProducto.php
+++ admin/catalogos/Productos/nuevoProducto.php
@@ -1,49 +1,50 @@
-<!-- Para enviar archivos es obligatorio el method="POST" y el enctype="multipart/form-data"-->
-<form id="frmProductos" class="w-40 m-auto mx-3 my-3" action="" method="GET" enctype="multipart/form-data">
+<form id="frmProductos" class="w-90 m-auto my-3" action="" method="POST" enctype="multipart/form-data">
     <input type="hidden" id="id" name="id">
     <div class="container">
         <div class="p-3 bd-highlight">
             <a href="index.php?page=productos" class="btn btn-secondary btn-regresar"><i class="fas fa-arrow-left"></i> Regresar</a>
         </div>
 
-        <div class="container">
-            <div>
-                <h2>Catálogo Menu</h2>
-            </div>
+        <div class="text-center mb-4">
+            <h2>Catálogo Menú</h2>
+        </div>
 
-            </div>
-            <div class="form-floating mb-3">
-                <input type="text" class="form-control" name="nombre" id="nombre" placeholder="nombre">
+        <div class="mb-3">
+            <div class="form-floating">
+                <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre">
                 <label for="nombre">Nombre</label>
             </div>
+        </div>
 
-            <div class="form-floating mb-3">
-                <input type="text" class="form-control" name="costo" id="costo" placeholder="costo">
+        <div class="mb-3">
+            <div class="form-floating">
+                <input type="text" class="form-control" name="costo" id="costo" placeholder="Costo">
                 <label for="costo">Costo</label>
             </div>
-            
-            <div class="form-floating mb-3">
-                <input type="text" class="form-control" name="tipo" id="tipo" placeholder="tipo">
+        </div>
+
+        <div class="mb-3">
+            <div class="form-floating">
+                <input type="text" class="form-control" name="tipo" id="tipo" placeholder="Tipo">
                 <label for="tipo">Tipo</label>
             </div>
+        </div>
 
-            <div class="form-floating mb-3">
-                <input type="text" class="form-control" name="duracion" id="duracion" placeholder="duracion">
-                <label for="duracion">Duracion</label>
-            </div>
-
-            <div class="mb-3">
-                <div>
-                    <label for="imagen">Imagen</label>
-                    <input type="file" class="form-control" name="imagen" id="imagen" accept="image/*">
-                    
-                </div>
-            </div>
-
-            <div class="mb-3">
-                <button id="guardar" type="button" class="btn btn-outline-dark">Guardar</button>
-                <button id="cancelar" type="reset" class="btn btn-outline-dark">Cancelar</button>
+        <div class="mb-3">
+            <div class="form-floating">
+                <input type="text" class="form-control" name="duracion" id="duracion" placeholder="Duración">
+                <label for="duracion">Duración</label>
             </div>
         </div>
-    
+
+        <div class="mb-3">
+            <label for="imagen">Imagen</label>
+            <input type="file" class="form-control" name="imagen" id="imagen" accept="image/*">
+        </div>
+
+        <div class="mb-3">
+            <button id="guardar" type="button" class="btn btn-outline-dark">Guardar</button>
+            <button id="cancelar" type="reset" class="btn btn-outline-dark">Cancelar</button>
+        </div>
+    </div>
 </form>
